06fcec472300a6df23ef7fed5f7217cd26ceaad1
Alex Hunziker <alex.hunziker@eawag.ch>
2016-10-13 10:43:25 +0200

    Return of Map function

--- map.r
+++ map.r
@@ -10,33 +10,36 @@
 # Returns: mapped_data, which includes the nodes with position of nodes, frequency and average errors
 #   as well as the error and winning node for each node of the testdata
 
-gsom.map <- function(gsom_model, newdf) {
+gsom.map <- function(newdf, gsom_model) {
   
   nodes <- list(
     position = gsom_model$nodes$position,
     weight = gsom_model$nodes$weight,
-    error = 0,
-    freq = 0
+    avgerror = rep(0, nrow(gsom_model$nodes$position)),
+    freq = rep(0, nrow(gsom_model$nodes$position))
   )
   
-  observation <- list(
+  observation <- data.frame(
     winner = numeric(),
     error = numeric()
   )
   
   mapped_data <- list(nodes = nodes, observation = observation)
   
+  newdf <- t(apply(newdf, 1, function(x){(x-gsom_model$norm_param$min)/(gsom_model$norm_param$max-gsom_model$norm_param$min)}))
+  
   for(j in 1:nrow(newdf)){
     errors <- sqrt(rowSums(sweep(gsom_model$nodes$weight, MARGIN = 2, newdf[j,], FUN="-")^2, dims=1))
     
     minError=min(errors)
     winner <- which(grepl(minError, errors))
+    #print(paste("Map:", winner))
     if(length(winner)>1) winner <- winner[1]
     
-    mapped_data$nodes$freq[winner] <- gsom_model$nodes$freq[winner] + 1
+    mapped_data$nodes$freq[winner] <- mapped_data$nodes$freq[winner] + 1
     mapped_data$nodes$avgerror[winner] <- mapped_data$nodes$avgerror[winner] + minError
     
-    mapped_data$observation[winner,] <- c(winner = winner, error = minError)
+    mapped_data$observation[j,] <- c(winner = winner, error = minError)
   }
   
   mapped_data$nodes$avgerror <- mapped_data$nodes$avgerror / mapped_data$nodes$freq
